aebb37938b7629d275ac4467177cedc79eb33a36
Kakao <mirtza911@rambler.ru>
2015-07-14 11:18:35 +0400

    мелкие правки

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,17 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  #def check_user 
+  #if current_user != nil
+ 	  #    if current_user.id == article.user_id || (current_user.admin? )
+     #    check_it = true
+
+     #   else check_it = false
+     #   end
+            
+  #end
+#end
+
+
 end
